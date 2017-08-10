ba64432e63186a669923e269d05f3b3358504fda
diahron <diahron@gmail.com>
2017-08-10 03:25:50 -0500

    Added pop out sidebar menu to right side.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
     render() {
         return (
             <div className="App">
                 <AppBar
                     title="Title"
                     iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={() => this.setState({open: !this.setState.open})}
                 />
+                <Drawer
+                    open={this.state.open}
+                    docked={false}
+                    onRequestChange={(open) => this.setState({open})}
+                >
+                    <MenuItem>Menu Item</MenuItem>
+                    <MenuItem>Menu Item 2</MenuItem>
+                </Drawer>
             </div>
-        );
+        )
     }
 }
 
